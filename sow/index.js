60a8230aabb860307bc12c2b72e805a8e761192b
StarTensor <1058989643@qq.com>
2026-01-22 23:11:12 +0800

    Update 20260122(3)

--- sow/index.js
+++ sow/index.js
@@ -5,6 +5,7 @@ window.onload = function() {
     window.document.getElementById("rt2").style.width = "100vw";
     window.document.getElementById("rb").style.width = "calc(100vw - 192px)";
     window.document.getElementById("rb3").src =  src="https://api.mcstatus.io/v2/widget/java/nbc.rainplay.cn:25970?dark=true&rounded=true";
+    window.document.getElementById("rb3").style.width = "min(384px, calc(100vw - 192px))";
 }
 
 let lDisplay = false;
@@ -19,6 +20,7 @@ function rt11() {
         window.document.getElementById("rt1").style.width = "calc(100vw - 260px)";
         window.document.getElementById("rt2").style.width = "calc(100vw - 260px)";
         window.document.getElementById("rb").style.width = "calc(100vw - 452px)";
+        window.document.getElementById("rb3").width = "min(384px, calc(100vw - 192px))";
     } else {
         lDisplay = false;
         window.document.getElementById("l").style.display = "none";
@@ -26,5 +28,6 @@ function rt11() {
         window.document.getElementById("rt1").style.width = "100vw";
         window.document.getElementById("rt2").style.width = "100vw";
         window.document.getElementById("rb").style.width = "calc(100vw - 192px)";
+        window.document.getElementById("rb3").style.width = "min(384px, calc(100vw - 192px))";
     }
 }
