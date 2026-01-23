66afc3020b548ca4ec40dd24aa4f2c723447e352
StarTensor <1058989643@qq.com>
2026-01-23 21:43:20 +0800

    Update 20260123(6)

--- sow/index.js
+++ sow/index.js
@@ -1,4 +1,6 @@
 window.onload = function() {
+    let nowDate = new Date();
+    window.document.getElementById("rb121b").innerHTML = ((Date.parse(`${nowDate.getFullYear()}-${nowDate.getMonth() < 9 ? "0" : ""}${nowDate.getMonth() + 1}-${nowDate.getDate()}`) - Date.parse("2026-01-20")) / 86400000).toString();
     window.document.getElementById("rb11").src = "https://api.mcstatus.io/v2/widget/java/nbc.rainplay.cn:25970?dark=true&rounded=true";
 }
 
