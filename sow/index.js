dac8119d8df1fcf0170422665f375afe0b9c3bb8
StarTensor <1058989643@qq.com>
2026-01-23 16:57:02 +0800

    Update 20260123(3)

--- sow/index.js
+++ sow/index.js
@@ -1,10 +1,5 @@
 window.onload = function() {
-    window.document.getElementById("r").style.width = "100vw";
-    window.document.getElementById("rt1").style.width = "100vw";
-    window.document.getElementById("rt2").style.width = "100vw";
-    window.document.getElementById("rb").style.width = "calc(100vw - 192px)";
     window.document.getElementById("rb3").src = "https://api.mcstatus.io/v2/widget/java/nbc.rainplay.cn:25970?dark=true&rounded=true";
-    window.document.getElementById("rb3").style.width = "min(384px, calc(100vw - 192px))";
 }
 
 let lDisplay = false;
